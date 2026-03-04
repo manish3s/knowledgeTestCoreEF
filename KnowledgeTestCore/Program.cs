6e83e80c6405c9db7a6353f7534f0fbe499d7b96
manish3s <mpandey6434@gmail.com>
2026-03-04 22:59:40 +0530

    ok

--- KnowledgeTestCore/Program.cs
+++ KnowledgeTestCore/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration
         .GetConnectionString("DefaultConnection")));
 
-// ✅ 2. DI
+// ✅ 2. DI-- to check
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
